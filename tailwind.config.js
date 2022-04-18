3f1d294944e7fed0d86c775d306ce65365a487b1
dreamspice <basicuser00@protonmail.com>
2022-04-18 15:02:38 +0200

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bcg': '#1D3251',
+        'border-gray': '#5D708A',
+        'dif-grey': '#5A576B',
+      },
+    },
+  },
+  plugins: [],
+}
